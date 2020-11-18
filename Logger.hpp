04117063bc69998910448333ee1da738cc78f3c6
pkaselj <petar.kaselj.00@gmail.com>
2020-11-18 11:38:41 +0000

    Changed timestamp to UNIXtime:usec---text

--- Logger.hpp
+++ Logger.hpp
@@ -6,6 +6,8 @@
 #include<cstdio>
 #include<unistd.h>
 #include<cstdlib>
+#include<sys/stat.h>
+#include<sys/time.h>
 
 /*
 *
@@ -18,31 +20,31 @@
 class Logger
 {
     private:
+    std::string filepath;
     std::ofstream output;
     time_t time;
 
     public:
-    Logger(const char* path);
+    Logger(const std::string path);
     ~Logger(void);
     void Crash(void);
+    bool exists(void);
+    void close(void);
+    void open(void);
 
     template <class T>
     friend Logger& operator <<(Logger& logStream, T const& report);
 };
 
-Logger::Logger(const char* path)
+Logger::Logger(const std::string path)
 {
-    output.open(path, std::ios::out | std::ios::ate);
-
-    if(output.is_open() == false)
-        std::cout << "Could not open log file!" << std::endl;
-
-    output << "Logger " + std::string(path) + " created!" << std::endl;
+    filepath = path;
+    open();
 }
 
 Logger::~Logger(void)
 {
-    output.close();
+    close();
 }
 
 void Logger::Crash(void)
@@ -51,13 +53,46 @@ void Logger::Crash(void)
     exit(-1);
 }
 
+bool Logger::exists(void)
+{
+    struct stat buf; // TODO Unnecessary buf?
+    if(stat(filepath.c_str(), &buf) >= 0)
+        return true;
+    return false;
+}
+
+void Logger::close(void)
+{
+    output.close();
+}
+
+void Logger::open(void)
+{
+    output.open(filepath.c_str(), std::ios::out | std::ios::ate);
+    if(output.is_open() == false)
+        std::cout << "Could not open log file!" << std::endl;
+
+    output << "Logger " + std::string(filepath) + " created!" << std::endl;
+}
+
 template<class T>
 Logger& operator <<(Logger& logStream, T const& report)
 {
+    if(logStream.exists() == false)
+    {
+        std::cout << "File doesn't exist!" << std::endl;
+    }
+    else if(logStream.output.is_open() == false)
+    {
+        logStream.open();
+    }
+
     if(logStream.output.is_open() == true)
     {
-        time(&logStream.time);
-        logStream.output << ctime(&logStream.time) << report << std::endl;
+        struct timeval time;
+        gettimeofday( &time, NULL );
+
+        logStream.output << time.tv_sec << ":" << time.tv_usec << "--- " << report << std::endl;
     }
 
     return logStream;
